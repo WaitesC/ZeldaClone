420c22a4f4a477e2caf82f489681239a8c35b5b1
Chris <cwaites@hotmail.com>
2020-09-26 17:41:07 +1000

    Added controller support and cinemachine camera

---
+++ Assets/Scripts/EnemyTargeter.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargeter : MonoBehaviour
+{
+    public GameObject closestEnemy;
+    public float closestEnemyDistance;
+
+    public float lockOnDistance = 5;
+    public bool lockedOnToEnemy;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        closestEnemy = FindClosestEnemy();
+        closestEnemyDistance = DistancePlayerEnemy();
+
+        if(DistancePlayerEnemy() < lockOnDistance)
+        {
+            if (Input.GetAxis("Lock On") != 0f)
+            {
+                FindClosestEnemy().gameObject.transform.localScale = new Vector3(1, 2, 1);
+                lockedOnToEnemy = true;
+            }
+
+            if (Input.GetAxis("Lock On") != 1f)
+            {
+                FindClosestEnemy().gameObject.transform.localScale = new Vector3(1, 1, 1);
+                lockedOnToEnemy = false;
+            }
+        }
+
+        if (DistancePlayerEnemy() > lockOnDistance)
+        {
+
+            FindClosestEnemy().gameObject.transform.localScale = new Vector3(1, 1, 1);
+            lockedOnToEnemy = false;
+            //
+        }
+    }
+
+    //finds closest enemy
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    //finds distance between player and closest enemy
+    public float DistancePlayerEnemy()
+    {
+        float enemyDistance = Vector3.Distance(FindClosestEnemy().transform.position, transform.position);
+
+
+        return enemyDistance;
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    float speedMultiplier = 1;
+
+
+    public float smoothTurnTime = 0.1f;
+    float smoothTurnVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        SprintCheck();
+    }
+
+    void Movement()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(hor, 0f, ver).normalized;
+
+        if (dir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime * speedMultiplier);
+        }
+    }
+
+    void SprintCheck()
+    {
+        if (Input.GetAxis("Sprint") != 0f)
+        {
+            speedMultiplier = 5;
+        }
+
+        if (Input.GetAxis("Sprint") != 1f)
+        {
+            speedMultiplier = 1;
+        }
+    }
+}
